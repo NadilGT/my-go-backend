6b09b659655bb57c5f0ba7a3b3019c44b8f64c0f
NadilGT <rccnadildinsara@gmail.com>
2025-09-28 20:08:35 +0530

    add FindAll API and DAO to return list of brands

---
+++ dto/model_Brand.go
@@ -0,0 +1,13 @@
+package dto
+
+import (
+	"time"
+)
+
+type Brand struct {
+	BrandId    string    `bson:"brandId" json:"brandId"`
+	Name       string    `bson:"name" json:"name"`
+	CategoryID string    `bson:"categoryId" json:"categoryId"`
+	CreatedAt  time.Time `bson:"created_at" json:"created_at"`
+	UpdatedAt  time.Time `bson:"updated_at" json:"updated_at"`
+}
