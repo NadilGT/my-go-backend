08505be45d8a70218768a7e1cecf538c1d1af690
NadilGT <rccnadildinsara@gmail.com>
2025-09-28 14:19:39 +0530

    Add a query params to create category

--- api/api_create_category.go
+++ api/api_create_category.go
@@ -13,30 +13,27 @@ import (
 func CreateCategoryApi(c *fiber.Ctx) error {
 	inputObj := dto.Category{}
 
-	// Parse request body
-	if err := c.BodyParser(&inputObj); err != nil {
-		return utils.SendErrorResponse(c, fiber.StatusBadRequest, err.Error())
+	categoryName := c.Query("categoryName")
+	if categoryName == "" {
+		return utils.SendErrorResponse(c, fiber.StatusBadRequest, "categoryName is required")
 	}
 
-	// Generate category ID
 	ctx := context.Background()
 	id, err := dao.GenerateId(ctx, "Categories", "CAT")
 	if err != nil {
 		return utils.SendErrorResponse(c, fiber.StatusInternalServerError, err.Error())
 	}
 
-	// Assign generated values
 	inputObj.CategoryId = id
+	inputObj.Name = categoryName
 	now := time.Now().UTC()
 	inputObj.CreatedAt = now
 	inputObj.UpdatedAt = now
 
-	// Save to DB
 	if err := dao.DB_CreateCategory(&inputObj); err != nil {
 		return utils.SendErrorResponse(c, fiber.StatusBadRequest, err.Error())
 	}
 
-	// Return success with created object
 	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
 		"success": true,
 		"data":    inputObj,
